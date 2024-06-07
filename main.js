41e46971c631cdd4119b2b511d65940512e4d58f
nhanluongoe <nhanlt18@gmail.com>
2024-06-07 14:57:45 +0700

    chore: remove unused code

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 import "./style.css";
 
-function getUserAgent() {
-  const userAgent = navigator.userAgent;
-  console.log(userAgent);
-  return userAgent;
-}
-
 document.querySelector("#user-agent").innerHTML = navigator.userAgent;
-
-getUserAgent();
